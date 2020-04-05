9cec131019961a815bfdee17230d3a95a886672c
sbhehe233 <55295768+sbhehe233@users.noreply.github.com>
2020-04-05 13:02:23 +0800

    Add files via upload

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,6 +14,7 @@ export default class Body extends React.Component {
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8" />
+                    <link rel="stylesheet" id="miyazaki-google-fonts-css" href="https://fonts.googleapis.com/css?family=Teko%3A400%2C500%2C600%2C700&amp;subset=latin-ext&amp;ver=4.9.2" type="text/css" media="all" />
                     <meta name="viewport" content="width=device-width, initialScale=1, userScalable=no" />
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')} />
                     <link rel="stylesheet" href={safePrefix('assets/css/markdown-images.css')} />
